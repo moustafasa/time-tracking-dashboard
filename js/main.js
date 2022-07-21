86db943a13388470f1a4ac0f86f241277e449527
moustafa <moustafasaad954@gmail.com>
2022-07-21 07:52:39 +0200

    start in main.js file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-let tabs = document.querySelector(".main .tabs");
-console.log(tabs);
-document.querySelector(".main .tabs").classList.add("this");
-console.log(tabs);
+let jsonFile = "js/data.json";
+fetch(jsonFile)
+  .then((data) => data.json())
+  .then((data) => {
+    let tabs = document.querySelector(".main .tabs");
+    let active = document.querySelector(".main .tabs .active").dataset.value;
+    let tasks = [...document.querySelectorAll(".task")];
+    tasks.forEach((task) => {
+      let now = document.querySelector(`.task.${task.classList[1]} .now`);
+      let previous = document.querySelector(
+        `.task.${task.classList[1]} .previous`
+      );
+    });
+    console.log(data);
+  });
